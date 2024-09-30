79bcc53f09cc1c17b674b43e43247096467104a4
Stax124 <tamoncz@gmail.com>
2024-09-30 23:13:39 +0200

    ✨ Change the hero text

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,7 +2,7 @@
   <UContainer class="mt-12">
     <PageHero
       title="Nuxt 3 + Supabase Starter"
-      description="Nuxt 3 + Supabase + FastAPI + Docker"
+      description="Nuxt 3 + Supabase + Cloudflare Pages"
     />
     <PrettyCard />
   </UContainer>
